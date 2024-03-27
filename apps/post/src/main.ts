bbe4a437cb799999140f4ffcc2fce4d434687646
Fiido <roccobosio@gmail.com>
2024-03-27 08:56:28 -0300

    dockerize app

--- apps/post/src/main.ts
+++ apps/post/src/main.ts
@@ -3,6 +3,6 @@ import { PostModule } from './post.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PostModule);
-  await app.listen(3000);
+  await app.listen(null);
 }
 bootstrap();
