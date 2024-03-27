5f96f2fb10e2d3e00eb6bb9bc72da8e592d4bf99
Fiido <roccobosio@gmail.com>
2024-03-27 13:24:10 -0300

    message consumer

--- apps/message/src/message.module.ts
+++ apps/message/src/message.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
+import { ConfigModule } from '@nestjs/config';
+import { RmqModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RmqModule,
+  ],
   controllers: [MessageController],
   providers: [MessageService],
 })
