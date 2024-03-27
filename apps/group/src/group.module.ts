248687686d0353ba468d0cc39d5f81cc4ece38b4
Fiido <roccobosio@gmail.com>
2024-03-27 13:22:13 -0300

    group consumer

--- apps/group/src/group.module.ts
+++ apps/group/src/group.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
+import { ConfigModule } from '@nestjs/config';
+import { RmqModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RmqModule,
+  ],
   controllers: [GroupController],
   providers: [GroupService],
 })
--- apps/group/src/main.ts
+++ apps/group/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { GroupModule } from './group.module';
+import { GROUP_SERVICE, RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(GroupModule);
+
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getRmqOptions(GROUP_SERVICE));
+
   await app.listen(null);
+  app.startAllMicroservices();
 }
 bootstrap();
