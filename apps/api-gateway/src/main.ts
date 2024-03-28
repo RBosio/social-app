358d94fef1b1c919d6ac5f378a6b47cadc78f935
Fiido <roccobosio@gmail.com>
2024-03-28 05:57:37 -0300

    validate user dtos

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
--- libs/common/src/dto/user/update-user.dto.ts
+++ libs/common/src/dto/user/update-user.dto.ts
@@ -1,5 +1,9 @@
+import { IsOptional } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsOptional()
   name?: string;
+
+  @IsOptional()
   password?: string;
-  image?: string;
 }
