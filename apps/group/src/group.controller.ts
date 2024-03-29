9b6028ee4db020e805e474d3daf1d57cc7b53f86
Fiido <roccobosio@gmail.com>
2024-03-29 06:09:52 -0300

    group consumer

--- apps/group/src/group.controller.ts
+++ apps/group/src/group.controller.ts
@@ -1,12 +1,82 @@
 import { Controller, Get } from '@nestjs/common';
 import { GroupService } from './group.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { CreateGroupDto, Group, RmqService, UpdateGroupDto } from '@app/common';
 
 @Controller()
 export class GroupController {
-  constructor(private readonly groupService: GroupService) {}
+  constructor(
+    private readonly groupService: GroupService,
+    private rmqService: RmqService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.groupService.getHello();
+  @MessagePattern({ cmd: 'find_groups' })
+  async findGroups(@Ctx() context: RmqContext): Promise<Group[]> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    return this.groupService.findGroups();
+  }
+
+  @MessagePattern({ cmd: 'find_group' })
+  async findGroup(
+    @Ctx() context: RmqContext,
+    @Payload() groupId: string,
+  ): Promise<Group> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    return this.groupService.findGroup(groupId);
+  }
+
+  @MessagePattern({ cmd: 'create_group' })
+  async createGroup(
+    @Ctx() context: RmqContext,
+    @Payload() data: { groupId: string; createGroupDto: CreateGroupDto },
+  ): Promise<string> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    await this.groupService.createGroup(data.groupId, data.createGroupDto);
+
+    return 'group created!';
+  }
+
+  @MessagePattern({ cmd: 'update_group' })
+  async updateGroup(
+    @Ctx() context: RmqContext,
+    @Payload() data: { groupId: string; updateGroupDto: UpdateGroupDto },
+  ): Promise<string> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    await this.groupService.updateGroup(data.groupId, data.updateGroupDto);
+
+    return 'group updated!';
+  }
+
+  @MessagePattern({ cmd: 'accept_friend' })
+  async acceptFriend(
+    @Ctx() context: RmqContext,
+    @Payload() groupId: string,
+  ): Promise<string> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    await this.groupService.acceptFriend(groupId);
+
+    return 'friend accepted!';
+  }
+
+  @MessagePattern({ cmd: 'delete_group' })
+  async deleteGroup(
+    @Ctx() context: RmqContext,
+    @Payload() groupId: string,
+  ): Promise<string> {
+    this.rmqService.acknowledgmentMessage(context);
+
+    await this.groupService.deleteGroup(groupId);
+
+    return 'group deleted!';
   }
 }
--- apps/group/src/group.service.ts
+++ apps/group/src/group.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { CreateGroupDto, GroupRepository, UpdateGroupDto } from '@app/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class GroupService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly groupRepository: GroupRepository) {}
+
+  async findGroups() {
+    return this.groupRepository.findAll();
+  }
+
+  async findGroup(groupId: string) {
+    const group = await this.groupRepository.findOneById(groupId);
+    if (!group)
+      throw new RpcException({
+        message: 'group not found',
+        status: HttpStatus.NOT_FOUND,
+      });
+
+    return group;
+  }
+
+  async createGroup(groupId: string, createGroupDto: CreateGroupDto) {
+    const group = this.groupRepository.create(createGroupDto);
+    group.id = groupId;
+
+    return this.groupRepository.save(group);
+  }
+
+  async updateGroup(groupId: string, updateGroupDto: UpdateGroupDto) {
+    const group = await this.findGroup(groupId);
+    
+    const groupUpdated = Object.assign(group, updateGroupDto);
+
+    return this.groupRepository.save(groupUpdated);
+  }
+
+  async acceptFriend(groupId: string) {
+    const group = await this.findGroup(groupId);
+    group.status = 'accepted';
+
+    return this.groupRepository.save(group);
+  }
+
+  async deleteGroup(groupId: string) {
+    return this.groupRepository.delete(groupId);
   }
 }
