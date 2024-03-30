3e62a76060bdd10d116f1d8c1b92e8d9f630b8b9
Fiido <roccobosio@gmail.com>
2024-03-30 04:29:04 -0300

    comment dtos - docs

--- libs/common/src/dto/comment/create-comment.dto.ts
+++ libs/common/src/dto/comment/create-comment.dto.ts
@@ -1,14 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCommentDto {
+  @ApiProperty({
+    name: 'comment',
+    description: 'The comment of the post',
+    type: 'string',
+    required: true,
+    example: 'This is a new comment',
+  })
   @IsNotEmpty()
   @IsString()
   comment: string;
 
+  @ApiProperty({
+    name: 'userId',
+    description: 'The user id',
+    type: 'string',
+    required: true,
+    example: '2d4d6481-37b1-4db6-b675-9c08a89b81af',
+  })
   @IsNotEmpty()
   @IsString()
   userId: string;
 
+  @ApiProperty({
+    name: 'postId',
+    description: 'The post id',
+    type: 'string',
+    required: true,
+    example: '3a1b3468-a410-4a89-936a-81504cc137e1',
+  })
   @IsNotEmpty()
   @IsString()
   postId: string;
--- libs/common/src/dto/comment/update-comment.dto.ts
+++ libs/common/src/dto/comment/update-comment.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateCommentDto {
+  @ApiProperty({
+    name: 'comment',
+    description: 'The new comment of the post',
+    type: 'string',
+    required: false,
+    example: 'This is a new comment',
+  })
   @IsOptional()
   @IsString()
   comment?: string;
