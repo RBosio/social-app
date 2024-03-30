445c386dd7567de20b6e4fff8ad2dd8a2eceb009
Fiido <roccobosio@gmail.com>
2024-03-30 12:21:59 -0300

    user controller - unit tests

---
+++ apps/user/src/test/stubs/user.stub.ts
@@ -0,0 +1,18 @@
+import { User } from '@app/common';
+
+export const userStub = (): User => {
+  return {
+    id: 'cf8f8b31-5fec-4c5c-91ea-7cd2de4dec5e',
+    name: 'fido',
+    email: 'fido@gmail.com',
+    password: '123456',
+    image: null,
+    created_at: new Date('2024-03-30T12:00:00'),
+    deleted_at: null,
+    comments: [],
+    groups: [],
+    likedPosts: [],
+    posts: [],
+    messages: [],
+  };
+};
---
+++ apps/user/src/test/user.controller.spec.ts
@@ -0,0 +1,140 @@
+import { Test } from '@nestjs/testing';
+import { UserController } from '../user.controller';
+import { UserService } from '../user.service';
+import { CreateUserDto, RmqService, UpdateUserDto, User } from '@app/common';
+import { userStub } from './stubs/user.stub';
+
+jest.mock('../user.service');
+describe('UserController', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [],
+      controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: RmqService,
+          useValue: {
+            acknowledgmentMessage: jest.fn().mockReturnValue(null),
+          },
+        },
+      ],
+    }).compile();
+
+    userController = moduleRef.get<UserController>(UserController);
+    userService = moduleRef.get<UserService>(UserService);
+    jest.clearAllMocks();
+  });
+
+  describe('findUsers', () => {
+    describe('when findUsers is called', () => {
+      let users: User[] = [];
+
+      beforeEach(async () => {
+        users = await userController.findUsers({} as any);
+      });
+
+      test('then it should call userService', () => {
+        expect(userService.findUsers).toHaveBeenCalled();
+      });
+
+      test('then it should return users', () => {
+        expect(users).toEqual([userStub()]);
+      });
+    });
+  });
+
+  describe('findUser', () => {
+    describe('when findUser is called', () => {
+      let user: User;
+
+      beforeEach(async () => {
+        user = await userController.findUser({} as any, userStub().id);
+      });
+
+      test('then it should call userService', () => {
+        expect(userService.findUserById).toHaveBeenCalledWith(userStub().id);
+      });
+
+      test('then it should return a user', () => {
+        expect(user).toEqual(userStub());
+      });
+    });
+  });
+
+  describe('createUser', () => {
+    describe('when createUser is called', () => {
+      const createUserDto: CreateUserDto = {
+        name: userStub().name,
+        email: userStub().email,
+        password: userStub().password,
+      };
+      let response: string;
+
+      beforeEach(async () => {
+        response = await userController.createUser({} as any, {
+          userId: userStub().id,
+          createUserDto,
+        });
+      });
+
+      test('then it should call userService', () => {
+        expect(userService.createUser).toHaveBeenCalledWith(
+          userStub().id,
+          createUserDto,
+        );
+      });
+
+      test("then it should return 'user created!' message", () => {
+        expect(response).toEqual('user created!');
+      });
+    });
+  });
+
+  describe('updateUser', () => {
+    describe('when updateUser is called', () => {
+      const updateUserDto: UpdateUserDto = {
+        name: userStub().name,
+      };
+      let response;
+      beforeEach(async () => {
+        response = await userController.updateUser({} as any, {
+          userId: userStub().id,
+          updateUserDto,
+        });
+      });
+
+      test('then it should call userService', () => {
+        expect(userService.updateUser).toHaveBeenCalledWith(
+          userStub().id,
+          updateUserDto,
+        );
+      });
+
+      test("then it should return 'user updated!' message", () => {
+        expect(response).toEqual('user updated!');
+      });
+    });
+  });
+
+  describe('deleteUser', () => {
+    describe('when deleteUser is called', () => {
+      let response: string;
+
+      beforeEach(async () => {
+        response = await userController.deleteUser({} as any, userStub().id);
+      });
+
+      test('then it should call userService', () => {
+        expect(userService.deleteUser).toHaveBeenCalledWith(userStub().id);
+      });
+
+      test("then it should return 'user deleted!' message", () => {
+        expect(response).toEqual('user deleted!');
+      });
+    });
+  });
+});
