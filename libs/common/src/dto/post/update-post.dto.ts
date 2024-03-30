b77e03f15e59ae996684d6a569ea25e362424085
Fiido <roccobosio@gmail.com>
2024-03-30 03:56:55 -0300

    post dtos - docs

--- libs/common/src/dto/post/update-post.dto.ts
+++ libs/common/src/dto/post/update-post.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdatePostDto {
+  @ApiProperty({
+    name: 'description',
+    description: 'The new description of the post',
+    type: 'string',
+    required: false,
+    example: 'This is a new post description',
+  })
   @IsOptional()
   @IsString()
   description?: string;
