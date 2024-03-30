9fa65ed25bb363d0c76dba8d770efa18d71f77f5
Fiido <roccobosio@gmail.com>
2024-03-30 04:48:12 -0300

    group dtos - docs

--- libs/common/src/dto/group/update-group.dto.ts
+++ libs/common/src/dto/group/update-group.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsOptional, IsString } from 'class-validator';
 
 export class UpdateGroupDto {
+  @ApiProperty({
+    name: 'name',
+    description: 'The name of the group',
+    required: false,
+    type: 'string',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({
+    name: 'usersId',
+    description: 'The users id of the group',
+    required: false,
+    type: 'string',
+  })
   @IsOptional()
   @IsArray()
   usersId?: number[];
