cecb492fe1242b14b01feedb9334ca4f5613d9ce
Fiido <roccobosio@gmail.com>
2024-04-05 11:30:27 -0300

    rate limiting

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -4,18 +4,31 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { GroupModule } from './group/group.module';
 import { MessageModule } from './message/message.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     UserModule,
     PostModule,
     GroupModule,
     MessageModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class ApiGatewayModule {}
