317e5d7e239c64b2f63e9e6adbdd8dd7037336bb
Fiido <roccobosio@gmail.com>
2024-04-05 11:15:50 -0300

    send mail

--- apps/mail/src/mail.module.ts
+++ apps/mail/src/mail.module.ts
@@ -2,14 +2,34 @@ import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
 import { RmqModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        MAIL_USER: Joi.string().required(),
+        MAIL_PASS: Joi.string().required(),
+      }),
     }),
     RmqModule,
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: configService.get('MAIL_USER'),
+            pass: configService.get('MAIL_PASS'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [MailController],
   providers: [MailService],
--- apps/mail/src/mail.service.ts
+++ apps/mail/src/mail.service.ts
@@ -1,8 +1,21 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MailService {
+  constructor(
+    private configService: ConfigService,
+    private mailerService: MailerService,
+  ) {}
+
   sendMail(email: string) {
-    console.log(`Sending mail to ${email}`);
+    this.mailerService.sendMail({
+      to: email,
+      from: this.configService.get('MAIL_USER'),
+      subject: 'Bienvenido',
+      text: `Bienvenido a nuestra plataforma.`,
+      html: `<b>Bienvenido a nuestra plataforma.</b>`,
+    });
   }
 }
